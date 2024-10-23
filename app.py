d1032e4aafac38d983627c6dcd660706010f4a22
ritafolisi <ritafolisi@studenti.unimi.it>
2024-10-23 17:16:08 +0200

    Second commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def just_run():
 
 
 app.run(host='0.0.0.0')     # bindati all'indirizzo ip 0, cioè a ciò che docker ti indirizzerà a runtime
-# Così binda anche su http://10.0.2.15:5000/ e non solo localhost
+# Così binda anche su http://10.0.2.15:5000/ e non solo localhost!
 
 
 
